fb7b551c87ba04917cc001478a4639319e8425dc
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-09-24 12:21:01 +0200

    separated to ctl binary

--- gnosis-vpn-ctl/src/main.rs
+++ gnosis-vpn-ctl/src/main.rs
@@ -1,3 +1,37 @@
+use clap::Parser;
+use std::io;
+use std::io::Write;
+use std::os::unix::net;
+use std::path::Path;
+
+/// Gnosis VPN system service - offers interaction commands on Gnosis VPN to other applications.
+#[derive(Parser)]
+struct Cli {
+    /// communication socket name - will be created by installer and should be /run/gnosisvpn/service.sock
+    socket: String,
+
+    /// command to run
+    cmd: String,
+}
+
+fn run_command(socket: &String, cmd: &String) -> Result<(), io::Error> {
+    let res = Path::try_exists(Path::new(socket));
+
+    let mut sender = match res {
+        Ok(true) => net::UnixStream::connect(socket),
+        Ok(false) => Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "Daemon not running",
+        )),
+        Err(x) => Err(x),
+    }?;
+
+    return sender.write_all(cmd.as_bytes());
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let args = Cli::parse();
+    let res = run_command(&args.socket, &args.cmd);
+    log::info!("patthern: {:?}, result: {:?}", args.socket, res);
 }
