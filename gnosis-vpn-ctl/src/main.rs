7f6b8dc43cdd18719b885f7afa92b18a08663f2d
Ronny Esterluss <esterlus@users.noreply.github.com>
2024-09-25 13:12:16 +0200

    Basic message sending via unix socket from ctl to daemon (#1)
    * separated projects
    
    * initial service
    
    * fixed daemon binary
    
    * separated to ctl binary
    
    * making top level workspace virtual
    
    * reacting to ctrl c but still busy looping
    
    * error handling with anyhow
    
    * better formatting
    
    * formatted code
    
    * adding crossbeam channels
    
    * got ctrl c working with channels
    
    * daemon mode no longer clocks cpu
    
    * added better logging
    
    * better logs handling
    
    * fixed clippy issues
    
    * update README"
    
    * basic CI pipeline
    
    * fix clippy error
    
    * updated job name

---
+++ gnosis-vpn-ctl/src/main.rs
@@ -0,0 +1,40 @@
+use anyhow::{anyhow, Context};
+use clap::Parser;
+use std::io::Write;
+use std::os::unix::net;
+use std::path::Path;
+
+/// Gnosis VPN system service - offers interaction commands on Gnosis VPN to other applications.
+#[derive(Parser)]
+struct Cli {
+    /// communication socket name - will be created by installer and should be /run/gnosisvpn/service.sock
+    #[arg(short, long)]
+    socket: String,
+
+    /// command to run
+    cmd: String,
+}
+
+fn run_command(socket: &String, cmd: &String) -> anyhow::Result<()> {
+    let res = Path::try_exists(Path::new(socket));
+
+    let mut sender = match res {
+        Ok(true) => net::UnixStream::connect(socket).with_context(|| "unable to connect to socket"),
+        Ok(false) => Err(anyhow!(format!("gnosis-vpn not running"))),
+        Err(x) => Err(anyhow!(x)),
+    }?;
+
+    log::info!("sending command: {}", cmd);
+    sender.write_all(cmd.as_bytes())?;
+    Ok(())
+}
+
+fn main() {
+    env_logger::init();
+    let args = Cli::parse();
+    let res = run_command(&args.socket, &args.cmd);
+    match res {
+        Ok(_) => log::info!("stopped gracefully"),
+        Err(x) => log::error!("stopped with error: {}", x),
+    }
+}
---
+++ gnosis-vpn/src/main.rs
@@ -0,0 +1,94 @@
+use anyhow::{anyhow, Context};
+use clap::Parser;
+use std::fs;
+use std::io::Read;
+use std::os::unix::net;
+use std::path::Path;
+use std::thread;
+
+/// Gnosis VPN system service - offers interaction commands on Gnosis VPN to other applications.
+#[derive(Parser)]
+struct Cli {
+    /// communication socket name - will be created by installer and should be /run/gnosisvpn/service.sock
+    #[arg(short, long)]
+    socket: String,
+}
+
+fn ctrl_channel() -> anyhow::Result<crossbeam_channel::Receiver<()>> {
+    let (sender, receiver) = crossbeam_channel::bounded(100);
+    ctrlc::set_handler(move || {
+        let _ = sender.send(());
+    })?;
+
+    Ok(receiver)
+}
+
+fn incoming(mut stream: net::UnixStream) -> anyhow::Result<()> {
+    let mut buffer = [0; 128];
+    let size = stream.read(&mut buffer)?;
+    log::info!("incoming: {}", String::from_utf8_lossy(&buffer[..size]));
+    Ok(())
+}
+
+fn daemon(socket: &String) -> anyhow::Result<()> {
+    let ctrl_c_events = ctrl_channel()?;
+
+    let socket_path = Path::new(socket);
+    let res_exists = Path::try_exists(socket_path);
+
+    let listener = match res_exists {
+        Ok(true) => Err(anyhow!(format!("already running"))),
+        Ok(false) => net::UnixListener::bind(socket)
+            .with_context(|| format!("error binding listener to socket {}", socket)),
+        Err(x) => Err(anyhow!(x)),
+    }?;
+
+    let (sender, receiver) = crossbeam_channel::unbounded::<net::UnixStream>();
+
+    let sender = sender.clone();
+    thread::spawn(move || {
+        for stream in listener.incoming() {
+            _ = match stream {
+                Ok(stream) => sender
+                    .send(stream)
+                    .with_context(|| "failed to send stream to channel"),
+                Err(x) => {
+                    log::error!("error waiting for incoming message: {:?}", x);
+                    Err(anyhow!(x))
+                }
+            };
+        }
+    });
+
+    log::info!("started successfully in listening mode");
+    loop {
+        crossbeam_channel::select! {
+            recv(ctrl_c_events) -> _ => {
+                log::info!("shutting down");
+                break;
+            }
+            recv(receiver) -> stream => {
+                _ = match stream  {
+                    Ok(s) => {
+                incoming(s)
+                    },
+                    Err(x) => Err(anyhow!(x))
+
+                }
+            },
+        }
+    }
+
+    fs::remove_file(socket_path)?;
+    Ok(())
+}
+
+fn main() {
+    env_logger::init();
+    let args = Cli::parse();
+    let res = daemon(&args.socket);
+    match res {
+        Ok(_) => log::info!("stopped gracefully"),
+        Err(x) => log::error!("stopped with error: {}", x),
+    }
+}
