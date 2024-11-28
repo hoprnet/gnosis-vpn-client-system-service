7059e7933e6301e63e6f0efa9f36d429666a3308
Ronny Esterluss <ronny.esterluss@gmail.com>
2024-11-28 13:43:40 +0100

    adjusted nix flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "GnosisVPN system service";
+  description = "hoprnet monorepo";
 
   inputs = {
     flake-utils.url = github:numtide/flake-utils;
@@ -65,6 +65,7 @@
               (fs.fileFilter (file: file.name == "Cargo.toml") ./.)
             ];
           };
+
           src = fs.toSource {
             root = ./.;
             fileset = fs.unions [
@@ -118,195 +119,36 @@
             crossSystem = pkgs.lib.systems.examples.armv7l-hf-multiplatform;
           };
 
-          hoprdBuildArgs = {
+          gnovpnBuildArgs = {
             inherit src depsSrc rev;
-            cargoExtraArgs = "-p hoprd-api";
-            cargoToml = ./hoprd/hoprd/Cargo.toml;
+            cargoExtraArgs = "-p gnosis-vpn";
+            cargoToml = ./gnosis-vpn/Cargo.toml;
           };
 
-          hoprd = rust-builder-local.callPackage ./nix/rust-package.nix hoprdBuildArgs;
-          hoprd-x86_64-linux = rust-builder-x86_64-linux.callPackage ./nix/rust-package.nix hoprdBuildArgs;
-          hoprd-aarch64-linux = rust-builder-aarch64-linux.callPackage ./nix/rust-package.nix hoprdBuildArgs;
-          hoprd-armv7l-linux = rust-builder-armv7l-linux.callPackage ./nix/rust-package.nix hoprdBuildArgs;
-          # CAVEAT: must be built from a darwin system
-          hoprd-x86_64-darwin = rust-builder-x86_64-darwin.callPackage ./nix/rust-package.nix hoprdBuildArgs;
-          # CAVEAT: must be built from a darwin system
-          hoprd-aarch64-darwin = rust-builder-aarch64-darwin.callPackage ./nix/rust-package.nix hoprdBuildArgs;
-
-          hopr-test = rust-builder-local.callPackage ./nix/rust-package.nix (hoprdBuildArgs // {
-            runTests = true;
-          });
-
-          hopr-test-nightly = rust-builder-local-nightly.callPackage ./nix/rust-package.nix (hoprdBuildArgs // {
-            runTests = true;
-            cargoExtraArgs = "-Z panic-abort-tests";
-          });
-
-          hoprd-clippy = rust-builder-local.callPackage ./nix/rust-package.nix (hoprdBuildArgs // { runClippy = true; });
-          hoprd-debug = rust-builder-local.callPackage ./nix/rust-package.nix (hoprdBuildArgs // {
-            CARGO_PROFILE = "dev";
-          });
-
-          hopliBuildArgs = {
+          gnovpnctlBuildArgs = {
             inherit src depsSrc rev;
-            cargoToml = ./hopli/Cargo.toml;
-            postInstall = ''
-              mkdir -p $out/ethereum/contracts
-              cp ethereum/contracts/contracts-addresses.json $out/ethereum/contracts/
-            '';
+            cargoExtraArgs = "-p gnosis-vpn-ctl";
+            cargoToml = ./gnosis-vpn-ctl/Cargo.toml;
           };
 
-          hopli = rust-builder-local.callPackage ./nix/rust-package.nix hopliBuildArgs;
-          hopli-x86_64-linux = rust-builder-x86_64-linux.callPackage ./nix/rust-package.nix hopliBuildArgs;
-          hopli-aarch64-linux = rust-builder-aarch64-linux.callPackage ./nix/rust-package.nix hopliBuildArgs;
-          hopli-armv7l-linux = rust-builder-armv7l-linux.callPackage ./nix/rust-package.nix hopliBuildArgs;
+          gnovpn = rust-builder-local.callPackage ./nix/rust-package.nix gnovpnBuildArgs;
+          gnovpnctl = rust-builder-local.callPackage ./nix/rust-package.nix gnovpnctlBuildArgs;
+
+          gnovpn-x86_64-linux = rust-builder-local.callPackage ./nix/rust-package.nix gnovpnBuildArgs;
+          gnovpnctl-x86_64-linux = rust-builder-local.callPackage ./nix/rust-package.nix gnovpnctlBuildArgs;
+          # hoprd-x86_64-linux = rust-builder-x86_64-linux.callPackage ./nix/rust-package.nix hoprdBuildArgs;
+          # hoprd-aarch64-linux = rust-builder-aarch64-linux.callPackage ./nix/rust-package.nix hoprdBuildArgs;
+          # hoprd-armv7l-linux = rust-builder-armv7l-linux.callPackage ./nix/rust-package.nix hoprdBuildArgs;
           # CAVEAT: must be built from a darwin system
-          hopli-x86_64-darwin = rust-builder-x86_64-darwin.callPackage ./nix/rust-package.nix hopliBuildArgs;
+          # hoprd-x86_64-darwin = rust-builder-x86_64-darwin.callPackage ./nix/rust-package.nix hoprdBuildArgs;
           # CAVEAT: must be built from a darwin system
-          hopli-aarch64-darwin = rust-builder-aarch64-darwin.callPackage ./nix/rust-package.nix hopliBuildArgs;
+          # hoprd-aarch64-darwin = rust-builder-aarch64-darwin.callPackage ./nix/rust-package.nix hoprdBuildArgs;
 
-          hopli-clippy = rust-builder-local.callPackage ./nix/rust-package.nix (hopliBuildArgs // { runClippy = true; });
-          hopli-debug = rust-builder-local.callPackage ./nix/rust-package.nix (hopliBuildArgs // {
-            CARGO_PROFILE = "dev";
-          });
+          gnovpn-clippy = rust-builder-local.callPackage ./nix/rust-package.nix (gnovpnBuildArgs // { runClippy = true; });
+          gnovpnctl-clippy = rust-builder-local.callPackage ./nix/rust-package.nix (gnovpnctlBuildArgs // { runClippy = true; });
+          gnovpn-debug = rust-builder-local.callPackage ./nix/rust-package.nix (gnovpnBuildArgs // { CARGO_PROFILE = "dev"; });
+          gnovpnctl-debug = rust-builder-local.callPackage ./nix/rust-package.nix (gnovpnctlBuildArgs // { CARGO_PROFILE = "dev"; });
 
-          profileDeps = with pkgs; [
-            gdb
-            # FIXME: heaptrack would be useful, but it adds 700MB to the image size (unpacked)
-            # lldb
-            rust-bin.stable.latest.minimal
-            valgrind
-          ];
-          # FIXME: the docker image built is not working on macOS arm platforms
-          # and will simply lead to a non-working image. Likely, some form of
-          # cross-compilation or distributed build is required.
-          hoprdDockerArgs = package: deps: {
-            inherit pkgs;
-            name = "hoprd";
-            extraContents = [
-              package
-            ] ++ deps;
-            Entrypoint = [
-              "/bin/hoprd"
-            ];
-          };
-          hoprd-docker = import ./nix/docker-builder.nix (hoprdDockerArgs hoprd [ ]);
-          hoprd-debug-docker = import ./nix/docker-builder.nix (hoprdDockerArgs hoprd-debug [ ]);
-          hoprd-profile-docker = import ./nix/docker-builder.nix (hoprdDockerArgs hoprd profileDeps);
-
-          hopliDockerArgs = package: deps: {
-            inherit pkgs;
-            name = "hopli";
-            extraContents = [
-              package
-            ] ++ deps;
-            Entrypoint = [
-              "/bin/hopli"
-            ];
-            env = [
-              "ETHERSCAN_API_KEY=placeholder"
-              "HOPLI_CONTRACTS_ROOT=${package}/ethereum/contracts"
-            ];
-          };
-          hopli-docker = import ./nix/docker-builder.nix (hopliDockerArgs hopli [ ]);
-          hopli-debug-docker = import ./nix/docker-builder.nix (hopliDockerArgs hopli-debug [ ]);
-          hopli-profile-docker = import ./nix/docker-builder.nix (hopliDockerArgs hopli profileDeps);
-
-          anvilSrc = fs.toSource {
-            root = ./.;
-            fileset = fs.unions [
-              (fs.fileFilter (file: file.hasExt "sol") ./vendor/solidity)
-              ./ethereum/contracts
-              ./scripts/run-local-anvil.sh
-            ];
-          };
-          anvil-docker = pkgs.dockerTools.buildLayeredImage {
-            name = "hopr-anvil";
-            tag = "latest";
-            # breaks binary reproducibility, but makes usage easier
-            created = "now";
-            contents = [ pkgs.foundry-bin anvilSrc pkgs.tini pkgs.runtimeShellPackage ];
-            enableFakechroot = true;
-            fakeRootCommands = ''
-              #!${pkgs.runtimeShell}
-              /scripts/run-local-anvil.sh
-              sleep 2
-              lsof -i :8545 -s TCP:LISTEN -t | xargs -I {} -n 1 kill {} || :
-              rm -rf /ethereum/contracts/broadcast/
-              rm -f /tmp/*.log
-              rm -f /.anvil.state.json
-            '';
-            config = {
-              Cmd = [
-                "/bin/tini"
-                "--"
-                "/scripts/run-local-anvil.sh"
-                "-s"
-                "-f"
-              ];
-            };
-          };
-          dockerImageUploadScript = image: pkgs.writeShellScriptBin "docker-image-upload" ''
-            set -eu
-            OCI_ARCHIVE="$(nix build --no-link --print-out-paths ${image})"
-            ${pkgs.skopeo}/bin/skopeo copy --insecure-policy \
-              --dest-registry-token="$GOOGLE_ACCESS_TOKEN" \
-              "docker-archive:$OCI_ARCHIVE" "docker://$IMAGE_TARGET"
-          '';
-          hoprd-docker-build-and-upload = flake-utils.lib.mkApp {
-            drv = dockerImageUploadScript hoprd-docker;
-          };
-          hoprd-debug-docker-build-and-upload = flake-utils.lib.mkApp {
-            drv = dockerImageUploadScript hoprd-debug-docker;
-          };
-          hoprd-profile-docker-build-and-upload = flake-utils.lib.mkApp {
-            drv = dockerImageUploadScript hoprd-profile-docker;
-          };
-          hopli-docker-build-and-upload = flake-utils.lib.mkApp {
-            drv = dockerImageUploadScript hopli-docker;
-          };
-          hopli-debug-docker-build-and-upload = flake-utils.lib.mkApp {
-            drv = dockerImageUploadScript hopli-debug-docker;
-          };
-          hopli-profile-docker-build-and-upload = flake-utils.lib.mkApp {
-            drv = dockerImageUploadScript hopli-profile-docker;
-          };
-          docs = rust-builder-local-nightly.callPackage ./nix/rust-package.nix (hoprdBuildArgs // {
-            buildDocs = true;
-          });
-          smoke-tests = pkgs.stdenv.mkDerivation {
-            pname = "hoprd-smoke-tests";
-            version = hoprdCrateInfo.version;
-            src = fs.toSource {
-              root = ./.;
-              fileset = fs.unions [
-                (fs.fileFilter (file: file.hasExt "sol") ./ethereum/contracts/src)
-                ./tests
-                ./scripts
-                ./ethereum/contracts/foundry.toml.in
-                ./ethereum/contracts/remappings.txt
-              ];
-            };
-            buildInputs = with pkgs; [
-              foundry-bin
-              solcDefault
-              hopli-debug
-              hoprd-debug
-              python39
-            ];
-            buildPhase = ''
-              unset SOURCE_DATE_EPOCH
-              python -m venv .venv
-              source .venv/bin/activate
-              pip install -U pip setuptools wheel
-              pip install -r tests/requirements.txt
-            '';
-            checkPhase = ''
-              source .venv/bin/activate
-              python3 -m pytest tests/
-            '';
-            doCheck = true;
-          };
           pre-commit-check = pre-commit.lib.${system}.run {
             src = ./.;
             hooks = {
@@ -322,9 +164,9 @@
             };
             tools = pkgs;
           };
+
           defaultDevShell = import ./nix/shell.nix { inherit pkgs config crane pre-commit-check solcDefault; };
-          smoketestsDevShell = import ./nix/shell.nix { inherit pkgs config crane pre-commit-check solcDefault; extraPackages = with pkgs; [ hoprd-debug hopli-debug tcpdump ]; };
-          docsDevShell = import ./nix/shell.nix { inherit pkgs config crane pre-commit-check solcDefault; extraPackages = with pkgs; [ html-tidy pandoc ]; useRustNightly = true; };
+
           run-check = flake-utils.lib.mkApp {
             drv = pkgs.writeShellScriptBin "run-check" ''
               set -e
@@ -375,10 +217,6 @@
             programs.taplo.enable = true;
             settings.formatter.taplo.excludes = [ "./vendor/*" "./ethereum/contracts/*" ];
 
-            # FIXME: currently broken in treefmt
-            # programs.ruff.check = true;
-            # settings.formatter.ruff.check.excludes = [ "./vendor/*" ];
-
             settings.formatter.solc = {
               command = "sh";
               options = [
@@ -409,39 +247,29 @@
           };
 
           checks = {
-            inherit hoprd-clippy hopli-clippy;
+            inherit gnovpn-clippy gnovpnctl-clippy;
           };
 
           apps = {
-            inherit hoprd-docker-build-and-upload;
-            inherit hoprd-debug-docker-build-and-upload;
-            inherit hoprd-profile-docker-build-and-upload;
-            inherit hopli-docker-build-and-upload;
-            inherit hopli-debug-docker-build-and-upload;
-            inherit hopli-profile-docker-build-and-upload;
             inherit update-github-labels;
             check = run-check;
           };
 
           packages = {
-            inherit hoprd hoprd-debug hoprd-docker hoprd-debug-docker hoprd-profile-docker;
-            inherit hopli hopli-debug hopli-docker hopli-debug-docker hopli-profile-docker;
-            inherit hopr-test hopr-test-nightly;
-            inherit anvil-docker;
-            inherit smoke-tests docs;
-            inherit pre-commit-check;
-            inherit hoprd-aarch64-linux hoprd-armv7l-linux hoprd-x86_64-linux;
-            inherit hopli-aarch64-linux hopli-armv7l-linux hopli-x86_64-linux;
+            inherit gnovpn gnovpn-debug;
+            inherit gnovpnctl gnovpnctl-debug;
+            inherit gnovpn-x86_64-linux;
+            inherit gnovpnctl-x86_64-linux;
+            # inherit gnovpn-aarch64-linux gnovpn-armv7l-linux gnovpn-x86_64-linux;
+            # inherit gnovpnctl-aarch64-linux gnovpnctl-armv7l-linux gnovpnctl-x86_64-linux;
             # FIXME: Darwin cross-builds are currently broken.
             # Follow https://github.com/nixos/nixpkgs/pull/256590
-            inherit hoprd-aarch64-darwin hoprd-x86_64-darwin;
-            inherit hopli-aarch64-darwin hopli-x86_64-darwin;
-            default = hoprd;
+            # inherit hoprd-aarch64-darwin hoprd-x86_64-darwin;
+            # inherit hopli-aarch64-darwin hopli-x86_64-darwin;
+            default = gnovpn;
           };
 
           devShells.default = defaultDevShell;
-          devShells.smoke-tests = smoketestsDevShell;
-          devShells.docs = docsDevShell;
 
           formatter = config.treefmt.build.wrapper;
         };
