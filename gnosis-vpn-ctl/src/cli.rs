f777c8986d368996be65289c4c22dab56e3e5993
Ronny Esterluss <ronny.esterluss@gmail.com>
2024-12-02 14:19:12 +0100

    guarding cli and adding unit test

--- gnosis-vpn-ctl/src/cli.rs
+++ gnosis-vpn-ctl/src/cli.rs
@@ -23,6 +23,10 @@ pub enum Command {
             long,
             help(
                 "Listen host can be provided like this: \"<host>:<port>\" or any combination thereof, e.g.: \":port\"."
+            ),
+            guard(
+                valid_listen_host,
+                r#"must be in the form of ":<port>", "<host>" or "<host>:<port>""#
             )
         )]
         listen_host: Option<String>,
@@ -44,5 +48,48 @@ fn maxhop(hop: &Option<u8>) -> bool {
     match hop {
         Some(h) => *h <= 3,
         None => false,
+    }
+}
+
+fn valid_listen_host(listen_host: &Option<String>) -> bool {
+    match listen_host {
+        Some(lh) => {
+            let parts: Vec<&str> = lh.split(':').collect();
+            match parts.len() {
+                1 => url::Host::parse(parts[0]).is_ok(),
+                2 => {
+                    let host_ok = if parts[0].is_empty() {
+                        true
+                    } else {
+                        url::Host::parse(parts[0]).is_ok()
+                    };
+                    let port_ok = if let Ok(port) = parts[1].parse::<u16>() {
+                        u16::MIN <= port && port <= u16::MAX
+                    } else {
+                        false
+                    };
+                    host_ok && port_ok
+                }
+                _ => false,
+            }
+        }
+        None => true,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::valid_listen_host;
+    #[test]
+    fn test_valid_listen_host() {
+        assert!(valid_listen_host(&Some("0.0.0.0:60006".to_string())));
+        assert!(valid_listen_host(&Some(":60006".to_string())));
+        assert!(valid_listen_host(&Some("0.0.0.0".to_string())));
+        assert!(valid_listen_host(&Some("localhost:0".to_string())));
+        assert!(!valid_listen_host(&Some("".to_string())));
+        assert!(!valid_listen_host(&Some("localhost:".to_string())));
+        assert!(!valid_listen_host(&Some("localhost:abc".to_string())));
+        assert!(!valid_listen_host(&Some("localhost:65536".to_string())));
+        assert!(!valid_listen_host(&Some("localhost:-1".to_string())));
     }
 }
