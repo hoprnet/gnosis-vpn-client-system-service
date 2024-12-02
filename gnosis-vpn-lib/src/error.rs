fc99390d034cb82e24f3cf19a5f117907cf17cea
Ronny Esterluss <ronny.esterluss@gmail.com>
2024-12-03 00:08:58 +0100

    converted to thiserror

--- gnosis-vpn-lib/src/error.rs
+++ gnosis-vpn-lib/src/error.rs
@@ -1,11 +1,31 @@
 use std::io;
+use std::path;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum Error {
+    #[error("service not running")]
     ServiceNotRunning,
-    SocketPathIO(io::Error),
-    ConnectSocketIO(io::Error),
+    #[error("error accessing socket at `{socket_path:?}`: {error:?}")]
+    SocketPathIO {
+        socket_path: path::PathBuf,
+        error: io::Error,
+    },
+    #[error("error connecting socket at `{socket_path:?}`: {error:?}")]
+    ConnectSocketIO {
+        socket_path: path::PathBuf,
+        error: io::Error,
+    },
+    #[error("failed serializing command: {0}")]
     CommandSerialization(serde_json::Error),
-    WriteSocketIO(io::Error),
-    ReadSocketIO(io::Error),
+    #[error("error writing to socket at `{socket_path:?}`: {error:?}")]
+    WriteSocketIO {
+        socket_path: path::PathBuf,
+        error: io::Error,
+    },
+    #[error("error reading from socket at `{socket_path:?}`: {error:?}")]
+    ReadSocketIO {
+        socket_path: path::PathBuf,
+        error: io::Error,
+    },
 }
